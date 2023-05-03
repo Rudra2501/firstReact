f5062ebc3142df684106de29a96da6e0181e977a
Rudra2501 <rpr250102@gmail.com>
2023-05-03 21:17:24 +0530

    single UI

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,35 +1,51 @@
-import React, { useState } from "react";
-
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
-import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
-
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
-
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+  let expensesContent = null;
+  <p className="expenses-filter">No Expenses found for the selected year!</p>;
 
-  return (
-    <div>
-      <Card className="expenses">
-        <ExpensesFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
+  if (filteredExpenses.length === 1) {
+    expensesContent = [
+      filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={Math.random().toString()}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
-      </Card>
-    </div>
+      )),
+      <p className="expenses-filter">
+        Only single Expense here. Please add more...
+      </p>,
+    ];
+  } else if (filteredExpenses.length > 1) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={Math.random().toString()}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {expensesContent}
+    </Card>
   );
 };
-
 export default Expenses;
